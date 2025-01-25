51143fa7e95e4ce277b1d7a34c35081be50992f8
Mert Kalkancı <mertkalkanci05@hotmail.com>
2025-01-25 03:20:26 +0300

    find players dynamically on scene start

--- Assets/Scripts/Gameplay/StaticFan.cs
+++ Assets/Scripts/Gameplay/StaticFan.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class StaticFan : MonoBehaviour
 {
-    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private string playerTag = "Player";
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private float fanSpeed = 1f,fanArea = 3f;
     [SerializeField] private Transform fanTransformWithDirection;
     private RaycastHit raycastHit;
+    private Rigidbody playerRigidbody;
 
     void Start()
     {
-        
+        if(playerRigidbody == null)
+        {
+            playerRigidbody = GameObject.FindGameObjectWithTag(playerTag).GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
