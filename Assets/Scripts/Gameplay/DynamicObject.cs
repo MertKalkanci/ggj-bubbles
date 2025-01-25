7e6ead33b7016cefa1f717253d7e3ad9500a5c65
Mert Kalkancı <mertkalkanci05@hotmail.com>
2025-01-25 03:34:39 +0300

    dynamic object collision problem fix

--- Assets/Scripts/Gameplay/DynamicObject.cs
+++ Assets/Scripts/Gameplay/DynamicObject.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class DynamicObject : MonoBehaviour
 {
+    [SerializeField] private float maximumVelocity = 0.1f;
     [SerializeField] private Vector3 movementAxis;
     [SerializeField] private Rigidbody rb;
     private bool moveable = false;
     private Vector2 inputScreenPosition, inputScreenPositionOld;
-    private Vector3 oldPosition,newPosition;
+    private Vector3 oldPosition, newPosition, movementVector;
     public void StartMovement()
     {
         moveable = true;
@@ -22,24 +23,26 @@ public class DynamicObject : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(moveable)
+        if (moveable)
         {
-            if(!InputManager.instance.isPressed)
+            if (!InputManager.instance.isPressed)
             {
                 moveable = false;
                 return;
             }
             inputScreenPosition = InputManager.instance.inputScreenPosition;
-
-            Ray newRay = Camera.main.ScreenPointToRay(inputScreenPosition);
-            if (Physics.Raycast(newRay, out RaycastHit hit))
-            {
-                newPosition = hit.point;
-            }
-
-            Vector3 movementInput = newPosition - oldPosition;
-            rb.MovePosition(rb.position + Vector3.Scale(movementInput, movementAxis));
-            oldPosition = newPosition;
         }
+
+        Ray newRay = Camera.main.ScreenPointToRay(inputScreenPosition);
+        if (Physics.Raycast(newRay, out RaycastHit hit))
+        {
+            newPosition = hit.point;
+        }
+
+        movementVector += Vector3.Scale(newPosition - oldPosition, movementAxis);
+        rb.MovePosition(rb.position + Vector3.ClampMagnitude(movementVector, maximumVelocity));
+        movementVector -= Vector3.ClampMagnitude(movementVector, maximumVelocity);
+
+        oldPosition = newPosition;
     }
 }
