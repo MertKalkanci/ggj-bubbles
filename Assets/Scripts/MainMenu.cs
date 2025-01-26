2ace24a0cc33aa92da3df2d891a4d76475fb3f60
selindoga <73192627+selindoga@users.noreply.github.com>
2025-01-26 16:08:32 +0300

    finalize fix scene issues

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,7 @@ public class MainMenu : MonoBehaviour
     public GameObject MainMenuObj;
     public GameObject Levels;
 
-    public void LevelsClicked()
-    {
-        MainMenuObj.SetActive(false);
-        Levels.SetActive(true);
-    }
-
-    public void BackClicked()
-    {
-        MainMenuObj.SetActive(true);
-        Levels.SetActive(false);
-    }
+    
+    
     
 }
